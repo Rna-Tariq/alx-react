a899181507e239d0cd6a629adb8dddd4b897a30d
Rna341 <rna.tariq.aboouf@gmail.com>
2024-10-30 18:56:11 +0300

    Fixed typo

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-const rootNotifications = ReactDOM.createRoot(document.getElementById('root-notifications'));
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root"));
 rootNotifications.render(
   <React.StrictMode>
     <Notifications />
