48321d2e6845862fcb610944b1e05dc13fff2b37
Rna341 <rna.tariq.aboouf@gmail.com>
2024-11-03 14:17:36 +0200

    Created basic tests with four tests

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,16 +1,19 @@
 import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
 
-test('returns current year', () => {
-	expect(getFullYear()).toBe(2021);
-});
+describe('Utility Functions', () => {
+    test('getFullYear returns the current year', () => {
+        expect(getFullYear()).toBe(new Date().getFullYear());
+    });
 
-test('correct footer copy', () => {
-	expect(getFooterCopy(true)).toBe('Holberton School');
-	expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
-});
+    test('getFooterCopy returns "Holberton School" when the argument is true', () => {
+        expect(getFooterCopy(true)).toBe("Holberton School");
+    });
 
-test('returns right notification', () => {
-	expect(getLatestNotification()).toBe(
-		'<strong>Urgent Requirement</strong> - complete by EOD'
-	);
+    test('getFooterCopy returns "Holberton School main dashboard" when the argument is false', () => {
+        expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
+    });
+
+    test('getLatestNotification returns the expected notification string', () => {
+        expect(getLatestNotification()).toBe("<strong>Urgent requirement</strong> - complete by EOD");
+    });
 });
